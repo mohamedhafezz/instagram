a66f0befc094cd2007677a4e405184c6278c9163
Mohamed Hafez <m_hafez_95@hotmail.com>
2020-11-01 19:11:25 +0200

    Updates

---
+++ precache-manifest.075ab4ab6763ae8f370614b607a4e8d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac86cc6c0e011dce70a25ee8182e9ba",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "8d16fef201b80ff34e5f",
+    "url": "/instagram/static/css/main.901991f0.chunk.css"
+  },
+  {
+    "revision": "ca5c501c99aebdfb3fe8",
+    "url": "/instagram/static/js/2.65e4eab6.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/instagram/static/js/2.65e4eab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d16fef201b80ff34e5f",
+    "url": "/instagram/static/js/main.ac1ceaba.chunk.js"
+  },
+  {
+    "revision": "060ad16ba7f5db70f5a5",
+    "url": "/instagram/static/js/runtime-main.a87bf05b.js"
+  }
+]);
